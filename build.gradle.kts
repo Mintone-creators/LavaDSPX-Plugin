e575e17972db8d0f0839a72fefebb5c43bd1839a
appujet <sdipedit@gmail.com>
2025-06-29 10:16:08 +0530

    Update build.gradle.kts to use dynamic versioning and enhance publishing configuration

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,10 +2,16 @@ plugins {
     java
     `maven-publish`
     alias(libs.plugins.lavalink)
+    id("com.github.johnrengelman.shadow")
 }
 
+val pluginVersion = "0.0.5"
+val commitSha = System.getenv("GITHUB_SHA")?.take(7) ?: "unknown"
+val preRelease = System.getenv("PRERELEASE")?.toBoolean() ?: false
+val verName = if (preRelease) "$pluginVersion-$commitSha" else pluginVersion
+
 group = "me.devoxin.lavadspx.plugin"
-version = "0.0.5"
+version = verName
 
 lavalinkPlugin {
     name = "lavadspx-plugin"
@@ -20,26 +26,51 @@ java {
     }
 }
 
-tasks {
-    compileJava {
-        options.encoding = "UTF-8"
-    }
-}
-
 repositories {
-    maven {
-        setUrl("https://jitpack.io")
-    }
+    mavenCentral()
+    maven { url = uri("https://jitpack.io") }
 }
 
 dependencies {
     implementation("com.github.Devoxin:LavaDSPX:2.0.1")
 }
 
+tasks {
+    compileJava {
+        options.encoding = "UTF-8"
+    }
+    
+    jar {
+        enabled = false
+    }
+    
+    shadowJar {
+        archiveClassifier.set("")
+        configurations = listOf(project.configurations.runtimeClasspath.get())
+    }
+    
+    build {
+        dependsOn(shadowJar)
+    }
+}
+
 publishing {
+    repositories {
+        maven {
+            url = if (preRelease) {
+                uri("https://maven.appujet.site/snapshots")
+            } else {
+                uri("https://maven.appujet.site/releases")
+            }
+            credentials {
+                username = System.getenv("MAVEN_USERNAME")
+                password = System.getenv("MAVEN_PASSWORD")
+            }
+        }
+    }
     publications {
         create<MavenPublication>("maven") {
-            from(components["java"])
+            artifact(tasks.shadowJar.get())
         }
     }
 }
